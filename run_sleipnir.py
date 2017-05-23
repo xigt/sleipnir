72eb77a76c64dde672f32d1bda55a2476ebecdf9
Michael Wayne Goodman <goodman.m.w@gmail.com>
2017-05-23 11:26:35 -0700

    Add CORS support

--- run_sleipnir.py
+++ run_sleipnir.py
@@ -3,6 +3,18 @@
 from flask import Flask
 import sleipnir
 
+# maybe constrain this to known origins to avoid excessive requests
+cors_origin = '*'
+
 app = Flask(__name__)
 app.register_blueprint(sleipnir.v1, url_prefix='/v1')
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', cors_origin)
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Accept,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+    return response
+
+
 app.run(debug=True)
